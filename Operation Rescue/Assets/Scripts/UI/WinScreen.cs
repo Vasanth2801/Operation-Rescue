6c2119fbebd72add79ca7abd652a128d6dc871b2
Vasanth2801 <vasanthravichandran28@gmail.com>
2026-03-05 19:03:31 +0530

    Winscreen

--- Operation Rescue/Assets/Scripts/UI/WinScreen.cs
+++ Operation Rescue/Assets/Scripts/UI/WinScreen.cs
@@ -2,5 +2,13 @@ using UnityEngine;
 
 public class WinScreen : MonoBehaviour
 {
-    
+    [SerializeField] private GameObject winScreen;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            winScreen.SetActive(true);
+        }
+    }
 }
