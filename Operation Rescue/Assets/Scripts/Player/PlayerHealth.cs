d6341b41d6de30fd946a5f783ecd6abbd3064994
Vasanth2801 <vasanthravichandran28@gmail.com>
2026-03-05 18:52:41 +0530

    All Canvas

--- Operation Rescue/Assets/Scripts/Player/PlayerHealth.cs
+++ Operation Rescue/Assets/Scripts/Player/PlayerHealth.cs
@@ -1,22 +1,30 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] private int maxHealth = 100;
     [SerializeField] private int currentHealth;
+    [SerializeField] private Image healthBar;
+    [SerializeField] private GameObject gameOverPanel;
 
     void Start()
     {
         currentHealth = maxHealth;
     }
 
+    void Update()
+    {
+        healthBar.fillAmount = (float)currentHealth/maxHealth;
+    }
+
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
-
         if(currentHealth <= 0)
         {
             currentHealth = 0;
+            gameOverPanel.SetActive(true);
             Destroy(gameObject);
         }
     }
