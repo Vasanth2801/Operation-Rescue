26a23e66d29d21e439844703ef739daca84d13e4
Vasanth2801 <vasanthravichandran28@gmail.com>
2026-03-05 15:36:50 +0530

    Player movement and mouse aim with object pooling and shooting

---
+++ Operation Rescue/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    [SerializeField] private float speed = 5f;
+
+    [Header("References")]
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Camera cam;
+
+    [Header("Inputs")]
+    [SerializeField] private float moveInputX;
+    [SerializeField] private float moveInputY;
+    [SerializeField] private Vector2 mousePos;
+
+    void Update()
+    {
+        moveInputX = Input.GetAxis("Horizontal");
+        moveInputY = Input.GetAxis("Vertical");
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        MouseLook();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void  Move()
+    {
+        rb.linearVelocity = new Vector2(moveInputX * speed, moveInputY * speed);
+    }
+
+    void MouseLook()
+    {
+        Vector2 direction = mousePos - rb.position;
+        float angle = Mathf.Atan2(direction.y,direction.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
+    }
+}
